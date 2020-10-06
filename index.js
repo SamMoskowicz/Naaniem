380f6dccc080f0883a63a0ffacfb26f75a0e0a54
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-10-06 15:37:12 -0400

    changed audio url

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const play = document.getElementById('play')
 
 const start = 9 * 60 + 21
 const end = 10 * 60 + 58
-const audio = new Audio('/Naaniem/01 Moshol Naneeim - Music.mp3')
+const audio = new Audio('../Naaniem/01 Moshol Naneeim - Music.mp3')
 audio.currentTime = start
 audio.addEventListener('timeupdate', () =>
     audio.currentTime >= end ? (audio.currentTime = start) : 0
